993f06d7ef85e1e768340e717399f969513483e4
mamun ethan <mamunsikder73375@gmail.com>
2024-02-03 00:33:50 +0600

    home page creation done without responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+// const showNav = document.querySelector(".show-nav");
+// const closeNav = document.querySelector(".close-nav");
+// const nav = document.querySelector(".nav");
+
+// closeNav.addEventListener("click", () => {
+//   nav.classList.remove("right-0");
+//   nav.classList.add("right-[-50%]");
+// });
+
+// showNav.addEventListener("click", () => {
+//   nav.classList.remove("right-[-50%]");
+//   nav.classList.add("right-0");
+// });
