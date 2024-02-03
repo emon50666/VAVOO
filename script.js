b6c5a0a055f74a3774e90a0a5ecffb0bc06b10b3
Emon <emon50666@gmail.com>
2024-02-03 14:44:07 +0600

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-// const showNav = document.querySelector(".show-nav");
-// const closeNav = document.querySelector(".close-nav");
-// const nav = document.querySelector(".nav");
+// toggle menu in mobile
 
-// closeNav.addEventListener("click", () => {
-//   nav.classList.remove("right-0");
-//   nav.classList.add("right-[-50%]");
-// });
-
-// showNav.addEventListener("click", () => {
-//   nav.classList.remove("right-[-50%]");
-//   nav.classList.add("right-0");
-// });
+document.querySelector(".toggle_menu").addEventListener("click", function () {
+    let mobile_menu = document.querySelector(".mobile_menu");
+    if (mobile_menu.style.display === "none") {
+      mobile_menu.style.display = "block";
+      mobile_menu.style.top = "15%";
+    } else {
+      mobile_menu.style.display = "none";
+    }
+  });
+  // scroll down to fixed header
+  window.addEventListener("scroll", function () {
+    let header = document.querySelector(".myHeader");
+  
+    if (window.scrollY >= 100) {
+      // Add the 'fixed-header' class to make the header sticky
+      header.classList.add("fixed-header");
+    } else {
+      // Remove the 'fixed-header' class to unstick the header
+      header.classList.remove("fixed-header");
+    }
+  });
+  
